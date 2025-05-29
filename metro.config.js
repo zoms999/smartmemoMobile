075eeb229d19c6811de3a9db0ae0c31613fb8713
zoms999 <hongsamcool1@gmail.com>
2025-05-30 00:51:56 +0900

    설정 에러 수정 - Metro와 Webpack 설정 안정화

--- metro.config.js
+++ metro.config.js
@@ -3,17 +3,29 @@ const { getDefaultConfig } = require('@expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 
 // React Native Web 지원을 위한 설정 추가
-defaultConfig.resolver.alias = {
-  'react-native': 'react-native-web',
-  ...defaultConfig.resolver.alias,
-};
+if (defaultConfig.resolver.alias) {
+  defaultConfig.resolver.alias['react-native'] = 'react-native-web';
+} else {
+  defaultConfig.resolver.alias = {
+    'react-native': 'react-native-web',
+  };
+}
 
-defaultConfig.resolver.extensions = [
-  '.web.tsx',
-  '.web.ts',
-  '.web.jsx',
-  '.web.js',
-  ...defaultConfig.resolver.extensions,
-];
+// 웹 전용 확장자 추가
+const webExtensions = ['.web.tsx', '.web.ts', '.web.jsx', '.web.js'];
+if (defaultConfig.resolver.extensions) {
+  defaultConfig.resolver.extensions = [
+    ...webExtensions,
+    ...defaultConfig.resolver.extensions,
+  ];
+} else {
+  defaultConfig.resolver.extensions = [
+    ...webExtensions,
+    '.tsx',
+    '.ts',
+    '.jsx',
+    '.js',
+  ];
+}
 
 module.exports = defaultConfig; 
--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,16 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-typescript'],
-            plugins: ['react-native-web/babel'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react', 
+              '@babel/preset-typescript'
+            ],
+            plugins: [
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+              ['@babel/plugin-proposal-private-methods', { loose: true }],
+              ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
+            ],
           },
         },
       },
